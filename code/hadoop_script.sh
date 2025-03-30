8c18d6a5aa0931eee8983d00e662a3a94487d664
Mahmuuod <78619567+Mahmuuod@users.noreply.github.com>
2025-03-30 21:38:53 +0200

    Add files via upload

--- code/hadoop_script.sh
+++ code/hadoop_script.sh
@@ -1,3 +1,4 @@
+
 set -e
 HADOOP_HOME="/data/hadoop-3.3.6"
 HADOOP_CONF_DIR="$HADOOP_HOME/etc/hadoop"
@@ -19,57 +20,27 @@ case "$NODE" in
         sudo chown -R hadoop:hadoop /data/zookeeper/
         ;;
     "Worker1")
-        mkdir .ssh/ 
+        mkdir -p .ssh/ 
         ;;
     *)
         echo "Unknown node type: $NODE. No specific configuration applied."
         ;;
 esac
 
-case "$NODE" in
-    "Master1")
-    ssh-keyscan -H localhost worker1 master2 master3 >> ~/.ssh/known_hosts
-	sudo service ssh start 
-    ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@localhost 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@worker1  
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@master2 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@master3 
-        ;;
-    "Master2")
-    ssh-keyscan -H localhost worker1 master1 master3 >> ~/.ssh/known_hosts
-	sudo service ssh start 
-    ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@localhost 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@worker1  
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@master1 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@master3 
-        ;;
-    "Master3")
-    ssh-keyscan -H localhost worker1 master1 master2 >> ~/.ssh/known_hosts
-	sudo service ssh start 
-    ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@localhost 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@worker1 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@master2 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@master1 
+# Configure SSH
+while ! nc -z Worker1 22 || ! nc -z Master1 22 || ! nc -z Master2 22 || ! nc -z Master3 22; do
+    echo "Waiting for SSH access on all nodes..."
+    sleep 2
+done
+echo 'All SSH Are On '
 
-        ;;
-    "Worker1")
-
-    ssh-keyscan -H localhost master2 master1 master3 >> ~/.ssh/known_hosts
-	sudo service ssh start 
-    ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@master3 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub localhost  
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@master2 
-	sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@master1 
-    ;;
-    *)
-        echo "Unknown node type: $NODE. No specific configuration applied."
-        ;;
-esac
-
+ssh-keyscan -H master1 worker1 master2 master3 >> ~/.ssh/known_hosts
+ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa 
+sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@master1 
+sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@worker1  
+sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@master2 
+sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub hadoop@master3 
+cat ~/.ssh/id_rsa.pub >> /data/shared/authorized_keys
 
 echo export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64 >> .profile 
 echo export HADOOP_CONF_DIR=/data/hadoop-3.3.6/etc/hadoop/ >> .profile
@@ -82,7 +53,8 @@ case "$NODE" in
     "Master1" | "Master2" | "Master3")
         echo 'hdfs --daemon start journalnode && hdfs --daemon start namenode && yarn --daemon start resourcemanager && zkServer.sh start && hdfs --daemon start zkfc' >> .profile
         ;;
-    "Worker1")
+    Worker* )
+        cp /data/shared/authorized_keys ~/.ssh/authorized_keys
         echo 'hdfs --daemon start datanode && yarn --daemon start nodemanager' >> .profile
         ;;
     *)
@@ -107,39 +79,40 @@ case "$NODE" in
     echo 3 > /opt/zookeeper/data/myid 
     zkServer.sh start
         ;;
-    "Worker1")
+    Worker* )
         ;;
     *)
         echo "Unknown node type: $NODE. No specific configuration applied."
         ;;
 esac
 
-sleep 5
+
+echo 'all nodes are up '
 
 case "$NODE" in
     "Master1" ) 
+    while ! nc -z Master1 8485 || ! nc -z Master2 8485 || ! nc -z Master3 8485; do
+        echo "Waiting for Journal Nodes on all nodes..."
+        sleep 2
+    done
     hdfs namenode -format  
     hdfs --daemon start namenode  
     hdfs zkfc -formatZK 
     hdfs --daemon start zkfc 
     yarn --daemon start resourcemanager
         ;;
-    "Master2" ) 
-    sleep 30
+    "Master2" | "Master3" ) 
+        while [ ! -d "/opt/hadoop/journal/mycluster" ]; do
+            echo "Waiting for Data Node To Be Formatted ..."
+            sleep 2
+        done
+    sleep 5
     hdfs namenode -bootstrapStandby
     hdfs --daemon start namenode
     yarn --daemon start resourcemanager
     hdfs --daemon start zkfc
         ;;
-    "Master3")
-    sleep 30
-    hdfs namenode -bootstrapStandby
-    hdfs --daemon start namenode  
-    yarn --daemon start resourcemanager
-    hdfs --daemon start zkfc
-        ;;
-    "Worker1")
-    sleep 30
+    Worker* )
     hdfs --daemon start datanode 
     yarn --daemon start nodemanager
         ;;
@@ -147,4 +120,5 @@ case "$NODE" in
         echo "Unknown node type: $NODE. No specific configuration applied."
         ;;
 esac
-su - hadoop 
+
+echo "$(hostname), Succeed"
